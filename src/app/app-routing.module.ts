e17e53b87234f538345dba773253d999fe2e1b7b
Mamady Camara <camaramamady9160@gmail.com>
2022-12-14 04:03:38 +0100

    accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
+import { DetailsComponent } from './details/details.component';
+import { RegionComponent } from './region/region.component';
 
 const routes: Routes = [
   { path: 'accueil', component: AccueilComponent },
+  { path: 'region', component: RegionComponent },
+  { path: 'details', component: DetailsComponent },
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,9 +11,10 @@ import {MatDividerModule} from '@angular/material/divider';
 import {MatCardModule} from '@angular/material/card';
 import { AccueilComponent } from './accueil/accueil.component';
 import { RegionComponent } from './region/region.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-  declarations: [AppComponent, AccueilComponent, RegionComponent],
+  declarations: [AppComponent, AccueilComponent, RegionComponent, DetailsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
