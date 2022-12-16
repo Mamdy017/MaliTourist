05ffdd90353e78700ccdee5d2d5291f4b338f034
Mamady Camara <camaramamady9160@gmail.com>
2022-12-16 01:16:43 +0100

    mise en place des pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { BoardComponent } from './board/board.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 import { DetailsComponent } from './details/details.component';
 import { RegionComponent } from './region/region.component';
 
 const routes: Routes = [
   { path: 'accueil', component: AccueilComponent },
   { path: 'region', component: RegionComponent },
-  { path: 'details', component: DetailsComponent },
   { path: 'board', component: BoardComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: 'connexion', component: ConnexionComponent },
 
 ];
 
